9e313fc240fd2aec0500e5990199836f5c5d56ce
Luke Lombardi <luke@beam.cloud>
2025-09-03 09:54:53 -0400

    log restore err

--- runc.go
+++ runc.go
@@ -717,6 +717,9 @@ func (r *Runc) Restore(context context.Context, id, bundle string, opts *Restore
 		opts.Started <- cmd.Process.Pid
 	}
 	status, err := Monitor.Wait(cmd, ec)
+	log.Println("status", status)
+	log.Println("ERROR FROM MONITOR err", err)
+	
 	if err == nil && status != 0 {
 		err = fmt.Errorf("%s did not terminate successfully: %w", cmd.Args[0], &ExitError{status})
 	}
